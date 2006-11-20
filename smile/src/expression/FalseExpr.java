7390614f1ff7f245665c8b5c9d031dde3afc6fab
hsi <hsi@informatik.uni-kiel.de>
2006-11-20 23:32:16 +0000

    Guards und Terms zu Expression<Typ> zusammengefaßt

---
+++ smile/src/expression/FalseExpr.java
@@ -0,0 +1,25 @@
+package expression;
+
+import csm.Dictionary;
+import csm.VarAssignment;
+import csm.Variable;
+import csm.exceptions.ErrUndefinedElement;
+
+
+public final class FalseExpr extends Expression<Boolean> {
+
+	@Override
+	public Boolean evaluate(VarAssignment va) {
+		return false;
+	}
+
+	@Override
+	public String prettyprint() {
+		return "false";
+	}
+
+	@Override
+	public void noUndefinedVars(Dictionary<Variable> variables)
+			throws ErrUndefinedElement {
+	}
+}
